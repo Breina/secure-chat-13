994f11cf6b004c05dfe0a2326e9738ff61e2798f
brecht.derwael@gmail.com <brecht.derwael@gmail.com@d870dd47-6815-3e9f-81b8-96623ae48011>
2014-02-04 21:34:59 +0000

    Test for Camera and GPS

--- trunk/SecureChat/src/alpha/android/fragments/OptionsFragment.java
+++ trunk/SecureChat/src/alpha/android/fragments/OptionsFragment.java
@@ -1,24 +1,58 @@
 package alpha.android.fragments;
 
-import com.google.android.gms.common.ConnectionResult;
-import com.google.android.gms.common.GooglePlayServicesUtil;
-
 import alpha.android.R;
-import alpha.android.common.CommonUtilities;
+import android.content.pm.PackageManager;
 import android.os.Bundle;
 import android.support.v4.app.Fragment;
-import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.CheckBox;
 
 public class OptionsFragment extends Fragment
 {
+
 	@Override
 	public View onCreateView(LayoutInflater inflater, ViewGroup container,
 			Bundle savedInstanceState)
 	{
-		return inflater.inflate(R.layout.fragment_content_options, container, false);
+		View view = inflater.inflate(R.layout.fragment_content_options,
+				container, false);
+
+		CheckBox cbCameraAllowed = (CheckBox) view
+				.findViewById(R.id.cb_allowCamera);
+		CheckBox cbGpsAllowed = (CheckBox) view.findViewById(R.id.cb_allowGPS);
+
+		if (checkCamera())
+			cbCameraAllowed.setChecked(true);
+
+		if (checkGPS())
+			cbGpsAllowed.setChecked(true);
+
+		return view;
+	}
+
+	private boolean checkCamera()
+	{
+		if (!getActivity().getPackageManager().hasSystemFeature(
+				PackageManager.FEATURE_CAMERA))
+			return false;
+
+		return true;
+	}
+
+	private boolean checkGPS()
+	{
+		if (!getActivity().getPackageManager().hasSystemFeature(
+				PackageManager.FEATURE_LOCATION))
+			return false;
+
+		if (getActivity().checkCallingOrSelfPermission(
+				"android.permission.ACCESS_FINE_LOCATION") == PackageManager.PERMISSION_GRANTED)
+			return true;
+
+		return false;
+
 	}
 
 }
